7341802851365485194f08f3e9bcbc7899a1c77d
G Dong <johndong9317@gmail.com>
2016-08-04 03:15:48 +0000

    add database

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
+var mongoose = require("mongoose");
+//cnct to mongo db
+mongoose.connect("mongodb://localhost/YelpCamp");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-var campgrounds = [
-      {name: "Jordan Lake", img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"},
-      {name: "Smoky Mounrain", img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"},
-      {name: "Jordan Lake", img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"},
-      {name: "Smoky Mounrain", img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"},
-      {name: "Jordan Lake", img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"},
-      {name: "Smoky Mounrain", img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"},
-      {name: "Jordan Lake", img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"},
-      {name: "Smoky Mounrain", img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"}
-   ];
+//create a simple schema for campgound
+var CampgroundSchema = new mongoose.Schema({
+   name: String,
+   img: String
+});
+//create model based on the schema
+var Campground = mongoose.model("Campground", CampgroundSchema);
+//create two inital data in the database;
+// Campground.create({
+//    name: "Jordan Lake",
+//    img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"
+// }, function(err, newCamp){
+//    if(err){
+//       console.log(err);
+//    } else {
+//       console.log("created first inital data");
+//    }
+// });
+// Campground.create({
+//    name: "Smoky Mounrain",
+//    img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"
+// }, function(err, newCamp){
+//    if(err){
+//       console.log(err);
+//    } else {
+//       console.log("created first inital data");
+//    }
+// });
 app.get("/", function(req, res){
    res.render("landing");
 });
 app.get("/campgrounds", function(req, res) {
-   
-   res.render("campgrounds", {campgrounds: campgrounds}); 
+   //get all campgrounds from data get render
+   Campground.find({}, function(err, allCampgrounds){
+      if (err) {
+         console.log(err);
+      } else {
+         res.render("campgrounds", {campgrounds: allCampgrounds});
+      }
+   });
 });
 app.post("/campgrounds", function(req, res) {
    //get date from the form and send it to campgrounds array
    var name = req.body.name;
    var img = req.body.img;
    var newCamp = {name: name, img: img};
-   campgrounds.push(newCamp);
-   //redirect to campground
-   res.redirect("/campgrounds");
+   Campground.create(newCamp, function(err, newC){
+      if (err) {
+         console.log(err);
+      } else {
+         //redirect to campground
+         res.redirect("/campgrounds");
+      }
+   });
 });
 app.get("/campgrounds/new", function(req, res) {
    res.render("new");
