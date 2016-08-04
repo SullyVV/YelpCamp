31cb4e5f775cbaf17293652ed2b5abcdc3e92f6e
G Dong <johndong9317@gmail.com>
2016-08-04 04:19:09 +0000

    add show template

--- app.js
+++ app.js
@@ -9,14 +9,16 @@ app.set("view engine", "ejs");
 //create a simple schema for campgound
 var CampgroundSchema = new mongoose.Schema({
    name: String,
-   img: String
+   img: String,
+   desc: String
 });
 //create model based on the schema
 var Campground = mongoose.model("Campground", CampgroundSchema);
 //create two inital data in the database;
 // Campground.create({
 //    name: "Jordan Lake",
-//    img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg"
+//    img: "https://www.meritagehomes.com/legacy/raleigh-nc/legacy-at-jordan-lake/jordan-lake-1871.jpg.mxw605.mxh311.jpg",
+//    desc: "Jordan Lake State Recreation Area is a North Carolina state park spanning Chatham County, and Wake County North Carolina in the United States. It comprises 4,558 acres of woodlands along the shores of 13,940-acre Jordan Lake."
 // }, function(err, newCamp){
 //    if(err){
 //       console.log(err);
@@ -26,7 +28,8 @@ var Campground = mongoose.model("Campground", CampgroundSchema);
 // });
 // Campground.create({
 //    name: "Smoky Mounrain",
-//    img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg"
+//    img: "https://www.visitnc.com/resimg.php/imgcrop/2/35625/image/800/448/Smokies.jpg",
+//    desc: "Great Smoky Mountains National Park straddles the border of North Carolina and Tennessee. The sprawling landscape encompases lush forests and an abundance of wildflowers that bloom year-round. Streams, rivers and waterfalls appear along hiking routes that include a segment of the Appalachian Trail. An observation tower tops Clingmans Dome, the highest peak, offering scenic views of the mist-covered mountains."
 // }, function(err, newCamp){
 //    if(err){
 //       console.log(err);
@@ -37,21 +40,24 @@ var Campground = mongoose.model("Campground", CampgroundSchema);
 app.get("/", function(req, res){
    res.render("landing");
 });
+//INDEX, show all campgrounds
 app.get("/campgrounds", function(req, res) {
    //get all campgrounds from data get render
    Campground.find({}, function(err, allCampgrounds){
       if (err) {
          console.log(err);
       } else {
-         res.render("campgrounds", {campgrounds: allCampgrounds});
+         res.render("index", {campgrounds: allCampgrounds});
       }
    });
 });
+//CREATE, add new campgrounds to db
 app.post("/campgrounds", function(req, res) {
    //get date from the form and send it to campgrounds array
    var name = req.body.name;
    var img = req.body.img;
-   var newCamp = {name: name, img: img};
+   var desc = req.body.desc;
+   var newCamp = {name: name, img: img, desc: desc};
    Campground.create(newCamp, function(err, newC){
       if (err) {
          console.log(err);
@@ -61,9 +67,22 @@ app.post("/campgrounds", function(req, res) {
       }
    });
 });
+//NEW, display form
 app.get("/campgrounds/new", function(req, res) {
    res.render("new");
 });
+//SHOW, show details about one campground
+app.get("/campgrounds/:id", function(req, res) {
+   //find the campground in index which triggers it 
+   //by using req.params.id
+   Campground.findById(req.params.id, function(err, foundID) {
+      if(err) {
+         console.log(err);
+      } else {
+         res.render("show", {foundCampground: foundID});
+      }
+   })
+})
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("YelpCamp server started!"); 
 });   
